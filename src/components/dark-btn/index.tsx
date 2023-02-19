9073b5567e234e78377126a4c482d41b2843bb17
j10c <blyb1739@gmail.com>
2023-02-19 22:41:35 +0800

    feat(home): 支持自适应系统夜间模式

--- src/components/dark-btn/index.tsx
+++ src/components/dark-btn/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import { memo } from "react";
 import styles from "./style.module.less";
 import { Button } from "antd";
@@ -6,18 +6,46 @@ interface IProps {
   children?: ReactNode;
 }
 
+const useBrowserTheme = () => {
+  const [theme, setTheme] = useState("light");
+  const media = useRef<MediaQueryList>();
+  if (typeof window !== "undefined") {
+    media.current = window.matchMedia("(prefers-color-scheme: dark)");
+  }
+
+  useEffect(() => {
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    setTheme(isDark? "dark": "light");
+  });
+
+  useEffect(() => {
+    media.current && media.current.addEventListener(
+      "change",
+      (e) => {
+        if (e.matches) {
+          setTheme("dark");
+        } else {
+          setTheme("light");
+        }
+      });
+  }, []);
+  return [theme];
+};
+
 const DarkBtn: React.FC<IProps> = () => {
   const [isDark, setIsDark] = useState(false);
   const [flag, setFlag] = useState(false);
+  const [browserTheme] = useBrowserTheme();
+
   const handleClick = () => {
     if (!isDark) localStorage.setItem("myDark", "1");
     else localStorage.setItem("myDark", "0");
     setIsDark(!isDark);
   };
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (document && localStorage.getItem("myDark") == null) {
-        document.body.className = "light";
+    if (typeof window !== "undefined" && document) {
+      if (localStorage.getItem("myDark") == null) {
+        document.body.className = browserTheme;
         setFlag(false);
       } else if (localStorage.getItem("myDark") == "1") {
         document.body.className = "dark";
@@ -26,8 +54,10 @@ const DarkBtn: React.FC<IProps> = () => {
         document.body.className = "light";
         setFlag(false);
       }
+    } else {
+      document.body.className = "light";
     }
-  }, [isDark]);
+  }, [isDark, browserTheme]);
   return (
     <div className={styles.darkContent} onClick={handleClick}>
       {!isDark && !flag ? (
